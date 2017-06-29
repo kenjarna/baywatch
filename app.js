d7201f8b16265a22f456f5fc308d5e7f9f377345
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-06-29 11:37:09 -0400

    Update removeButton functionality to remove the object from the DOM as well as the flick array

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = {
         document
             .querySelector(selectors.formSelector)
             .addEventListener('submit', this.handlesubmit.bind(this))
+
     },
 
     renderListItem(flick) {
         const item = document.createElement('li')
         item.textContent = flick.name
+        item.dataset.flickname = flick.name
         const likebutton = this.renderLikeButton()
         const removebutton = this.renderRemoveButton()
         item.appendChild(likebutton)
@@ -22,7 +24,9 @@ const app = {
         const button = document.createElement("button")
         button.setAttribute("class", "warning button")
         button.innerHTML = "Like"
-        button.setAttribute("onclick", "this.parentElement.style.backgroundColor = '#ffae00'")
+        button.addEventListener('click', function() {
+            this.parentElement.style.backgroundColor = '#ffae00'
+        })
         return button
     },
 
@@ -30,7 +34,15 @@ const app = {
         const button = document.createElement("button")
         button.setAttribute("class", "alert button")
         button.innerHTML = "DELETE"
-        button.setAttribute("onclick", "this.parentElement.parentElement.removeChild(this.parentElement)")
+        button.addEventListener('click', function(ev) {
+            const listelement = ev.target.parentElement
+            const flickname = listelement.dataset.flickname
+            const index = this.flick_array.findIndex(function(flick) {
+                return flickname === flick.name
+            })
+            this.flick_array.splice(index, 1)
+            listelement.parentElement.removeChild(ev.target.parentElement)
+        }.bind(this))
         return button
     },
 
@@ -44,6 +56,7 @@ const app = {
         }
 
         const listItem = this.renderListItem(flick)
+        console.log(this.flick_array)
         this.flick_array.push(flick)
         this.list.appendChild(listItem)
         this.max++
